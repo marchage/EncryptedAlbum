2d8bc9432bf5e6b89356570f6a946877687a627d
marchage <marc.hage2@gmail.com>
2025-11-04 02:07:13 +0100

    Initial Commit

---
+++ SecretVault/SecretVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretVaultApp.swift
+//  SecretVault
+//
+//  Created by Marc Hage on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SecretVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
