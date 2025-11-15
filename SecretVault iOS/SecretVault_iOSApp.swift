3854feb1ecaebe5e07de4ce3795ae000cad16f72
marchage <marc.hage2@gmail.com>
2025-11-15 07:47:30 +0100

    feat: Add initial project structure with asset catalogs and main content view

---
+++ SecretVault iOS/SecretVault_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretVault_iOSApp.swift
+//  SecretVault iOS
+//
+//  Created by Marc Hage on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SecretVault_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
